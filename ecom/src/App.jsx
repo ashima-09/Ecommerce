a271ef772a508522594f3e2811982dc07609429b
kritika220703 <101122444+kritika220703@users.noreply.github.com>
2023-06-12 23:39:23 +0530

    chg1

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import ProductList from "./pages/ProductList"
 
 const App = () => {
   return (
-    <Navbar/>
+    <ProductList/>
   )
 }
 

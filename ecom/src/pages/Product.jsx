95eaf09b76d12e3d9eec5daa3772c280286576f5
kritika220703 <101122444+kritika220703@users.noreply.github.com>
2023-06-13 18:47:29 +0530

    chg4

--- ecom/src/pages/Product.jsx
+++ ecom/src/pages/Product.jsx
@@ -11,9 +11,11 @@ import {
 import { Button } from '@mui/material';
 
 const Container = styled.div``;  
+
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({padding: "10px", flexDirection: "column"})};
 `;  
 
 const ImgContainer = styled.div`
@@ -24,6 +26,7 @@ const Img = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    ${mobile({height: "40vh"})};
 `;  
 
 const Title = styled.h1`
@@ -33,6 +36,7 @@ const Title = styled.h1`
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+    ${mobile({padding: "10px"})};
 `;  
 
 const Desc = styled.p`
@@ -49,6 +53,7 @@ const FilterContainer = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    ${mobile({width: "100%"})};
 `;
 
 const Filter = styled.div`
@@ -82,6 +87,7 @@ const AddContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({width: "100%"})};
 `;
 
 const AmountContainer = styled.div`
